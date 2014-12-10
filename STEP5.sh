d28f4f7c992676e1c020b7da0c2a92e2c2fd6443
DebianJoe <jbpaladin27@gmail.com>
2014-12-10 13:53:34 +0800

    further along, reboot time

--- STEP5.sh
+++ STEP5.sh
@@ -5,3 +5,17 @@ cp /usr/ucb/echo /bin/echo
 
 cp /usr/bin/whoami /usr/bin/whoami-backup
 cp /usr/ucb/whoami /usr/bin/whoami
+# test step, pass
+
+cp /usr/bin/catman /usr/bin/catman-backup
+cp /usr/ucb/catman /usr/bin/catman
+
+rm /usr/ucb/catman /usr/ucb/whoami /usr/ucb/echo
+
+cp /bin/ls /bin/ls-backup
+cp /usr/ucb/ls /bin/ls
+
+cp /usr/bin/test /usr/bin/test-backup
+cp /usr/ucb/test /usr/bin/test
+
+rm /usr/ucb/test /usr/ucb/ls
