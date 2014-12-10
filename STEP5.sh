881a71d2d099d3e9b3f49c8e37d43c9b916c0f95
DebianJoe <jbpaladin27@gmail.com>
2014-12-10 14:24:08 +0800

    time to reboot

--- STEP5.sh
+++ STEP5.sh
@@ -19,3 +19,22 @@ cp /usr/bin/test /usr/bin/test-backup
 cp /usr/ucb/test /usr/bin/test
 
 rm /usr/ucb/test /usr/ucb/ls
+# test step, pass
+
+cp /usr/bin/basename /usr/bin/basename-backup
+cp /usr/ucb/basename /usr/bin/basename
+
+rm /usr/ucb/ls /usr/ucb/basename
+
+cp /bin/ln /bin/ln-backup
+cp /usr/ucb/ln /bin/ln
+# created test link, works.
+
+cp /bin/chown /bin/chown-backup
+cp /usr/ucb/chown /bin/chown
+
+rm /usr/ucb/ln /usr/ucb/chown
+
+cp /bin/stty /bin/stty-backup
+cp /usr/ucb/stty /bin/stty
+#test via full reboot at this step
