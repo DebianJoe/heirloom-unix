a0ef05487e09c1d735ce66ee1db7ba5005af6203
DebianJoe <jbpaladin27@gmail.com>
2014-12-11 18:17:33 +0800

    removal started

---
+++ STEP6.sh
@@ -0,0 +1,42 @@
+#!/bin/ush
+
+## this script removes backups made during step 5
+## must be ran as root...possible borkage if untested.
+
+rm /usr/bin/yes-backup
+rm /usr/bin/getopt-backup /usr/bin/expr-backup
+rm /usr/bin/du-backup /usr/bin/expand-backup
+rm /usr/bin/dirname-backup
+rm /usr/bin/getconf-backup
+rm /usr/bin/fmt-backup
+rm /usr/bin/diff-backup /usr/bin/diff3-backup
+rm /usr/bin/csplit-backup
+rm /usr/bin/cut-backup
+rm /usr/bin/comm-backup
+rm /usr/bin/file-backup /usr/bin/env-backup
+rm /usr/bin/cmp-backup /usr/bin/find-backup
+rm /usr/bin/col-backup /usr/bin/cksum-backup
+rm /usr/bin/fold-backup /usr/bin/cal-backup
+rm /usr/bin/head-backup /usr/bin/id-backup
+rm /usr/bin/join-backup /usr/bin/line-backup
+rm /usr/bin/unexpand-backup /usr/bin/logname-backup
+rm /usr/bin/man-backup /usr/bin/mesg-backup
+rm /usr/bin/mkfifo-backup /usr/bin/nice-backup
+rm /usr/bin/nl-backup /usr/bin/nohup-backup
+rm /usr/bin/od-backup /usr/bin/paste-backup
+rm /usr/bin/pathchk-backup
+rm /usr/bin/pr-backup /usr/bin/pg-backup
+rm /usr/bin/printenv-backup /usr/bin/pgrep-backup
+rm /usr/bin/renice-backup /usr/bin/sdiff-backup
+rm /usr/bin/printf-backup /usr/bin/sort-backup
+rm /usr/bin/split-backup /usr/bin/tail-backup
+rm /usr/bin/touch-backup /usr/bin/tr-backup
+rm /usr/bin/ul-backup /usr/bin/units-backup
+rm /usr/bin/uniq-backup /usr/bin/users-backup
+rm /usr/bin/tee-backup /usr/bin/tty-backup
+rm /usr/bin/who-backup /usr/bin/wc-backup
+rm /usr/bin/xargs-backup /usr/bin/tabs-backup
+rm /usr/bin/sum-backup /usr/bin/groups-backup
+rm /usr/bin/install-backup /usr/bin/basename-backup
+rm /usr/bin/test-backup /usr/bin/catman-backup
+rm /usr/bin/whoami-backup
